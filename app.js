b388edae387b74f0b2a5e8f2648e6ee736d326e0
Arun Poudel <arunpdl013@gmail.com>
2021-04-06 21:42:05 +0545

    mongo atlas and dotenv

--- app.js
+++ app.js
@@ -1,80 +1,89 @@
 ///////////////////////////SETUP/////////////////////////////////
-
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const mongoose = require("mongoose");
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
 const bodyParser = require("body-parser");
 const maindeck = require("./cards.json");
-const gameController  = require("./controllers/game.js");
+const gameController = require("./controllers/game.js");
 const socketHandler = require("./socketHandler.js")(io);
-const Lobby = require("./models/lobby.js")
+const Lobby = require("./models/lobby.js");
 
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
-mongoose.connect('mongodb://localhost:27017/cardgame', {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
 });
 
 const db = mongoose.connection;
 db.once("open", () => {
-    console.log("Database connected");
+  console.log("Database connected");
 });
 
 ////////////////////////////////////////////PAGE ROUTES////////////////////////////////////////
 
 //Serve index page
-app.get('/', gameController.index);
+app.get("/", gameController.index);
 
 //Serve game page
-app.get("/game", gameController.game)
+app.get("/game", gameController.game);
 
 //Join Game
 app.post("/game/join", gameController.join);
 
 //Create a new game
-app.post('/game/new', gameController.create);
+app.post("/game/new", gameController.create);
 
 //Serve cards page
-app.get('/cards', gameController.cards);
+app.get("/cards", gameController.cards);
 
 ////////////////////////////////////////////////SOCKET.IO SECTION/////////////////////////////////////////
 
 //Once a socket has connected
-io.on('connection', function (socket) {
+io.on("connection", function (socket) {
   console.log("user has connected: " + socket.id);
-  
-  socket.on("newUser",  async function(data) {
+
+  socket.on("newUser", async function (data) {
     socket.username = data.username;
     socket.lobbycode = data.lobbycode;
 
-    const lobby = await Lobby.findOne({ lobbyCode: socket.lobbycode}).populate({path: "players"});
+    const lobby = await Lobby.findOne({
+      lobbyCode: socket.lobbycode,
+    }).populate({ path: "players" });
     var isInLobby = false;
     var playerList = [];
 
-    lobby.players.forEach(player => {
-        playerList.push(player.username);
-        if (player.username == socket.username) {
-          player.socketid = socket.id;
-          isInLobby = true;
-        }
-        player.save();
+    lobby.players.forEach((player) => {
+      playerList.push(player.username);
+      if (player.username == socket.username) {
+        player.socketid = socket.id;
+        isInLobby = true;
+      }
+      player.save();
     });
 
-    if(isInLobby){ 
-        console.log("Adding: " + socket.username + " to: " + socket.lobbycode);
-        socket.join(socket.lobbycode);
-        io.to(socket.lobbycode).emit("connectedRoom", { room: data.room, players: playerList,username:socket.username});
-    }
-    else {
+    if (isInLobby) {
+      console.log("Adding: " + socket.username + " to: " + socket.lobbycode);
+      socket.join(socket.lobbycode);
+      io.to(socket.lobbycode).emit("connectedRoom", {
+        room: data.room,
+        players: playerList,
+        username: socket.username,
+      });
+    } else {
       console.log("User has been disconnected");
-      io.to(socket.username).emit("chat message", {sender: "GAME", msg: "Error, user disconnected, please return to the homepage and rejoin"})
+      io.to(socket.username).emit("chat message", {
+        sender: "GAME",
+        msg:
+          "Error, user disconnected, please return to the homepage and rejoin",
+      });
     }
   });
 
@@ -88,18 +97,14 @@ io.on('connection', function (socket) {
 
   socket.on("chat message", socketHandler.chatMsg);
 
-  socket.on('disconnect', socketHandler.disconnect);
+  socket.on("disconnect", socketHandler.disconnect);
 });
 
 ////////////////////////////////////////////////////Start Server + Catch All/////////////////////////////////////////
-app.get("*", function(req,res) {
+app.get("*", function (req, res) {
   res.send("Error, page not found");
-})
-
-http.listen(3024 ,function(){
-  console.log('listening on *:3024');
 });
 
-
-
-
+http.listen(3024, function () {
+  console.log("listening on *:3024");
+});
